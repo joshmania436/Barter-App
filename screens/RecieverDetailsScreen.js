9298e4b79c4d0bcb865d01616a57cb8c69bac543
Joshua <55012411+joshmania436@users.noreply.github.com>
2020-09-09 09:12:20 +0530

    Created RecieverDetailsScreen

--- screens/RecieverDetailsScreen.js
+++ screens/RecieverDetailsScreen.js
@@ -2,22 +2,22 @@ import React ,{Component} from 'react';
 import {View,Text,StyleSheet,TouchableOpacity} from 'react-native';
 import{Card,Header,Icon} from 'react-native-elements';
 import firebase from 'firebase';
-
+import { RFValue } from "react-native-responsive-fontsize";
 import db from '../config.js';
 
 export default class ReceiverDetailsScreen extends Component{
   constructor(props){
     super(props);
     this.state={
-      userId          : firebase.auth().currentUser.email,
-      userName          :'',
-      receiverId      : this.props.navigation.getParam('details')["username"],
-      exchangeId       : this.props.navigation.getParam('details')["exchangeId"],
-      itemName        : this.props.navigation.getParam('details')["item_name"],
-      description  : this.props.navigation.getParam('details')["description"],
-      receiverName    : '',
-      receiverContact : '',
-      receiverAddress : '',
+      userId : firebase.auth().currentUser.email,
+      userName :'',
+      receiverId : this.props.navigation.getParam('details')["username"],
+      exchangeId : this.props.navigation.getParam('details')["exchangeId"],
+      itemName : this.props.navigation.getParam('details')["item_name"],
+      description: this.props.navigation.getParam('details')["description"],
+      receiverName: '',
+      receiverContact: '',
+      receiverAddress: '',
       receiverRequestDocId : ''
     }
   }
@@ -94,36 +94,33 @@ componentDidMount(){
       <View style={styles.container}>
         <View style={{flex:0.1}}>
           <Header
-            leftComponent ={<Icon name='arrow-left' type='feather' color='#696969'  onPress={() => this.props.navigation.goBack()}/>}
-            centerComponent={{ text:"Exchange Items", style: { color: '#90A5A9', fontSize:20,fontWeight:"bold", } }}
-            backgroundColor = "#eaf8fe"
+            leftComponent ={<Icon name='arrow-left' type='feather' color='#ffff'  onPress={() => this.props.navigation.goBack()}/>}
+            centerComponent={{ text:"Exchange Items", style: { color:'#ffff', fontSize:20,fontWeight:"bold", } }}
+            backgroundColor = "#32867d"
           />
         </View>
-        <View style={{flex:0.3}}>
+        <View style={{flex:0.3,marginTop:RFValue(20)}}>
           <Card
               title={"Item Information"}
               titleStyle= {{fontSize : 20}}
             >
-            <Card >
+            
               <Text style={{fontWeight:'bold'}}>Name : {this.state.itemName}</Text>
-            </Card>
-            <Card>
+            
               <Text style={{fontWeight:'bold'}}>Reason : {this.state.description}</Text>
-            </Card>
+          
           </Card>
         </View>
         <View style={{flex:0.3}}>
           <Card
-            title={"receiver Information"}
+            title={"Receiver Information"}
             titleStyle= {{fontSize : 20}}
             >
             <Card>
               <Text style={{fontWeight:'bold'}}>Name: {this.state.receiverName}</Text>
-            </Card>
-            <Card>
+            
               <Text style={{fontWeight:'bold'}}>Contact: {this.state.receiverContact}</Text>
-            </Card>
-            <Card>
+            
               <Text style={{fontWeight:'bold'}}>Address: {this.state.receiverAddress}</Text>
             </Card>
           </Card>
@@ -139,7 +136,7 @@ componentDidMount(){
                     this.addNotification()
                     this.props.navigation.navigate('MyBarters')
                   }}>
-                <Text>I want to Exchange</Text>
+                <Text style={{color:'#ffff'}}>I want to Exchange</Text>
               </TouchableOpacity>
             )
             : null
@@ -159,7 +156,8 @@ const styles = StyleSheet.create({
   buttonContainer : {
     flex:0.3,
     justifyContent:'center',
-    alignItems:'center'
+    alignItems:'center',
+    marginTop:RFValue(30)
   },
   button:{
     width:200,
@@ -167,7 +165,7 @@ const styles = StyleSheet.create({
     justifyContent:'center',
     alignItems : 'center',
     borderRadius: 10,
-    backgroundColor: 'orange',
+    backgroundColor: '#32867d',
     shadowColor: "#000",
     shadowOffset: {
        width: 0,
