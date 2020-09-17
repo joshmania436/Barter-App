577912b8a5c031dbc7749c081d86a5147bd6a2de
Joshua <55012411+joshmania436@users.noreply.github.com>
2020-09-17 19:38:24 +0530

    Final Update

--- config.js
+++ config.js
@@ -1,18 +1,17 @@
 import firebase from 'firebase';
 require('@firebase/firestore')
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+  apiKey: "AIzaSyBJsTqwL8yjTWW8mrCWgrBird7HI944SIw",
+  authDomain: "book-santa.firebaseapp.com",
+  databaseURL: "https://book-santa.firebaseio.com",
+  projectId: "book-santa",
+  storageBucket: "book-santa.appspot.com",
+  messagingSenderId: "546422247592",
+  appId: "1:546422247592:web:a82af183e05f978755f29d",
+  measurementId: "G-MLCQNSGDLH"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-var firebaseConfig = {
-    apiKey: "AIzaSyBJsTqwL8yjTWW8mrCWgrBird7HI944SIw",
-    authDomain: "book-santa.firebaseapp.com",
-    databaseURL: "https://book-santa.firebaseio.com",
-    projectId: "book-santa",
-    storageBucket: "book-santa.appspot.com",
-    messagingSenderId: "546422247592",
-    appId: "1:546422247592:web:a82af183e05f978755f29d",
-    measurementId: "G-MLCQNSGDLH"
-  };
-  // Initialize Firebase
-  
-  firebase.initializeApp(firebaseConfig);
-
-  export default firebase.firestore();
+export default firebase.firestore()
